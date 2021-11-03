f3227063e62254606647bc15be2b402c0f9e7dea
Akshita Verma <akshitaverma102001@gmail.com>
2021-11-03 17:01:40 +0530

    register page added

--- models/user.js
+++ models/user.js
@@ -1,7 +1,11 @@
 var mongoose=require('mongoose')
 var UserSchema=new mongoose.Schema({
+    name:String,
+    sname:String,
+    dob:String,
+    email_id:String,
     username:String,
-    password:String
+    password:String,
 
 });
 module.exports=mongoose.model("User",UserSchema);
