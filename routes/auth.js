4fcad198d99400ef822d22895559a33f7a27b016
Akshita Verma <akshitaverma102001@gmail.com>
2021-11-07 02:37:31 +0530

    login created

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,6 @@ router.get("/register",function(req,res){
 router.post("/register",function(req,res){
    var name=req.body.name;
    var sname=req.body.sname;
-
    var email_id=req.body.email_id;
    var username=req.body.username;
    var password=req.body.password;
