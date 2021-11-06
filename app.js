9a56778d3db9bfb93440ace7dd5533950b0105bc
Akshita Verma <akshitaverma102001@gmail.com>
2021-11-07 02:32:28 +0530

    middleware added

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express             =require("express"),
  mongoose               =require("mongoose"),
  bodyParser             =require("body-parser"),
+ passport               =require("passport"),
+ LocalStrategy          =require("passport-local"),
+ passportLocalMongoose  =require("passport-local-mongoose"),
+ User                   =require("./models/user"),
  app                    =express()
 
  mongoose.connect("mongodb://localhost/train");
@@ -13,11 +17,37 @@ var bookroute=require("./routes/bookingrouter")
 var authroute=require("./routes/auth")
 
 
+app.use(require("express-session")({
+    secret:"COOOOOL COOOOL COOOL",
+    resave:false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser()); 
+passport.deserializeUser(User.deserializeUser()); 
+
+mongoose.Promise=global.Promise;
+
+
+
+app.use(function(req,res,next){
+    res.locals.currentuser=req.user;
+    next();
+});
+
+
+
 app.get("/",function(req,res){
-    res.render("home");
+    res.render("home",{currentuser:req.user});
 })
+
 app.use(bookroute);
 app.use(authroute);
+
 app.listen(process.env.PORT||2000,function(){
     console.log("running");
 });
