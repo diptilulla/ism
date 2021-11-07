7e4a2dc473344fb0a0554f163bd90dc8e0d720c3
Akshita Verma <akshitaverma102001@gmail.com>
2021-11-07 14:03:58 +0530

    GET used instead of POST

--- routes/auth.js
+++ routes/auth.js
@@ -6,19 +6,19 @@ var User            =require("../models/user");
 router.get('/users', function(req, res) {
     res.json({ currentUser: req.user });
 });
-router.get("/register",function(req,res){
+router.get("/register/new",function(req,res){
     res.render("register");
 
 });
-router.post("/register",function(req,res){
-   var name=req.body.name;
-   var sname=req.body.sname;
-   var email_id=req.body.email_id;
-   var username=req.body.username;
-   var password=req.body.password;
+router.get("/register",function(req,res){
+   var name=req.query.name;
+   var sname=req.query.sname;
+   var email_id=req.query.email_id;
+   var username=req.query.username;
+   var password=req.query.password;
 
     var newBooking=new User({name:name,sname:sname,email_id:email_id,username:username})
-    
+
     
     User.register(newBooking,password,function(err,user){
 
@@ -27,7 +27,8 @@ router.post("/register",function(req,res){
         console.log(err)
         return res.render("register")
     }
-            passport.authenticate("local")(req,res,function(){
+      console.log(user)      
+      passport.authenticate("local")(req,res,function(){
             res.redirect("/");
             console.log("i am working")
 
