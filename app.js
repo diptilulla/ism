ba6034f87dcf41b31b399e612d9bb97ae5da8b38
diptilulla <60360333+diptilulla@users.noreply.github.com>
2022-04-28 00:33:50 +0530

    setup

--- app.js
+++ app.js
@@ -1,69 +1,95 @@
-var express             =require("express"),
+var express = require("express"),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose"),
+  User = require("./models/user"),
+  app = express();
+require("dotenv").config();
 
- mongoose               =require("mongoose"),
- bodyParser             =require("body-parser"),
- passport               =require("passport"),
- LocalStrategy          =require("passport-local"),
- passportLocalMongoose  =require("passport-local-mongoose"),
- User                   =require("./models/user"),
- app                    =express();
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+var bookroute = require("./routes/bookingrouter");
+var authroute = require("./routes/auth");
 
- mongoose.connect("mongodb://localhost/train");
-app.set("view engine","ejs");
-app.use(express.static('./public'))
-app.use(bodyParser.urlencoded({extended:true}));
+// var RateLimit = require("express-rate-limit");
 
-var bookroute=require("./routes/bookingrouter")
-var authroute=require("./routes/auth")
+// const apiRequestLimiter = RateLimit({
+//   windowMs: 1 * 60 * 1000, // 1 minute
+//   max: 5 // limit each IP to 5 requests per windowMs
+// });
 
-var RateLimit = require('express-rate-limit');
- 
- 
-  
- const apiRequestLimiter = RateLimit({
-     windowMs: 1 * 60 * 1000, // 1 minute
-     max: 5 // limit each IP to 5 requests per windowMs
- })
- 
- // Use the limit rule as an application middleware
- app.use(apiRequestLimiter)
+// // Use the limit rule as an application middleware
+// app.use(apiRequestLimiter);
 
-app.use(require("express-session")({
-    secret:"COOOOOL COOOOL COOOL",
-    resave:false,
-    saveUninitialized: false,
-   
-}));
+app.use(
+  require("express-session")({
+    secret: "COOOOOL COOOOL COOOL",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser()); 
-passport.deserializeUser(User.deserializeUser()); 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-mongoose.Promise=global.Promise;
+mongoose.Promise = global.Promise;
 
-
-
-app.use(function(req,res,next){
-    res.locals.currentuser=req.user;
-    next();
+app.use(function (req, res, next) {
+  res.locals.currentuser = req.user;
+  next();
 });
 
+// let logins = {};
 
+// app.get("/", function (req, res) {
+//   const etag = req.headers["if-none-match"];
+//   let user = null;
+//   if (req.headers.cookie) {
+//     user = req.headers["cookie"].split("=")[1];
+//     createUserfromEtag(etag, user);
+//   } else {
+//     user = getUserFromEtag(etag);
+//     if (user) {
+//       res.setHeader("set-cookie", [`user=${user}`]);
+//     }
+//   }
+//   res.render("home", {
+//     currentuser: user
+//   });
+// });
 
-app.get("/",function(req,res){
-    res.render("home",{currentuser:req.user});
-})
+// function createUserfromEtag(etag, user) {
+//   logins[etag] = user;
+// }
 
+// function getUserFromEtag(etag) {
+//   return logins[etag];
+// }
+app.get("/", function (req, res) {
+  res.render("home", {
+    currentuser: req.headers["cookie"]
+      ? req.headers["cookie"].split("=")[1]
+      : null
+  });
+});
 app.use(bookroute);
 app.use(authroute);
 
-
-
-
-app.listen(process.env.PORT||2000,function(){
-    console.log("running");
-});
+//  mongoose.connect("mongodb://localhost/train");
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() =>
+    app.listen(process.env.PORT || 2000, () => console.log("Server running"))
+  )
+  .catch((err) => console.log(err.message));
--- routes/auth.js
+++ routes/auth.js
@@ -1,81 +1,157 @@
-var express          =require("express");
-var router           =express.Router();
-var passport         =require("passport");
-var User            =require("../models/user");
+// var express = require("express");
+// var router = express.Router();
+// var passport = require("passport");
+// var User = require("../models/user");
 
+// router.get("/users", function (req, res) {
+//   res.json({ currentUser: req.user });
+// });
+// router.get("/register", function (req, res) {
+//   res.render("register");
+// });
 
-router.get('/users', function(req, res) {
-    res.json({ currentUser: req.user });
+// router.post("/register", function (req, res) {
+//   //use post method
+//   var name = req.query.name;
+//   var sname = req.query.sname;
+//   var email_id = req.query.email_id;
+//   var username = req.query.username;
+//   var password = req.query.password;
+//   let alert = require("alert");
+//   var passCheck = false;
+//   var usernameCheck = false;
+
+//   if (!/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/.test(password)) {
+//     alert(
+//       "Choose a strong password! Password should contain at least 1 number and a special charcter and should be at least 6 characters long."
+//     );
+//   } else {
+//     passCheck = true;
+//   }
+//   if (!/^[a-zA-Z0-9]+$/i.test(username)) {
+//     alert("Username should contain only alphabets and numbers!");
+//   } else {
+//     usernameCheck = true;
+//   }
+//   if (passCheck && usernameCheck) {
+//     var newBooking = new User({
+//       name: name,
+//       sname: sname,
+//       email_id: email_id,
+//       username: username
+//     });
+//   }
+//   User.register(newBooking, password, function (err, user) {
+//     if (err) {
+//       console.log(err);
+//       return res.render("register");
+//     }
+//     console.log(user);
+//     passport.authenticate("local")(req, res, function () {
+//       res.redirect("/");
+//     });
+//   });
+// });
+
+// router.get("/login", function (req, res) {
+//   res.render("login");
+// });
+
+// router.post(
+//   "/login",
+//   passport.authenticate("local", {
+//     successRedirect: "/",
+//     failureRedirect: "/register"
+//   }),
+//   function (req, res) {}
+// );
+
+// router.get("/logout", function (req, res) {
+//   req.logout();
+//   res.redirect("/");
+// });
+
+// module.exports = router;
+
+var express = require("express");
+var router = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
+
+router.get("/users", function (req, res) {
+  res.json({ currentUser: req.user });
 });
-router.get("/register",function(req,res){
-    res.render("register");
-
+router.get("/register", function (req, res) {
+  res.render("register");
 });
 
+router.post("/register", function (req, res) {
+  //use post method
+  var name = req.query.name;
+  var sname = req.query.sname;
+  var email_id = req.query.email_id;
+  var username = req.query.username;
+  var password = req.query.password;
+  let alert = require("alert");
+  var passCheck = false;
+  var usernameCheck = false;
 
-
-router.get("/register",function(req,res){  //use post method
-    var name=req.query.name;
-    var sname=req.query.sname;
-    var email_id=req.query.email_id;
-    var username=req.query.username;
-    var password=req.query.password;
-    let alert = require('alert');
-    var passCheck= false;
-    var usernameCheck=false;
-    
-    if (!(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/).test(password)){
-        alert('Choose a strong password! Password should contain at least 1 number and a special charcter and should be at least 6 characters long.');
-        
-    }
-    else{
-        passCheck=true;
-    }
-    if(!(/^[a-zA-Z0-9]+$/i).test(username)){
-        alert('Username should contain only alphabets and numbers!');
-    }else{
-        usernameCheck=true;
-    }
-    if(passCheck && usernameCheck){
-        var newBooking=new User({name:name,sname:sname,email_id:email_id,username:username});
-    }
-    User.register(newBooking,password,function(err,user){
-
-    
-    if(err){
-        console.log(err)
-        return res.render("register")
-    }
-        console.log(user)      
-        passport.authenticate("local")(req,res,function(){
-            res.redirect("/");
-           
-        });
-        
-    
+  if (!/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/.test(password)) {
+    alert(
+      "Choose a strong password! Password should contain at least 1 number and a special charcter and should be at least 6 characters long."
+    );
+  } else {
+    passCheck = true;
+  }
+  if (!/^[a-zA-Z0-9]+$/i.test(username)) {
+    alert("Username should contain only alphabets and numbers!");
+  } else {
+    usernameCheck = true;
+  }
+  if (passCheck && usernameCheck) {
+    var newBooking = new User({
+      name: name,
+      sname: sname,
+      email_id: email_id,
+      username: username
     });
-
+  }
+  User.register(newBooking, password, function (err, user) {
+    if (err) {
+      console.log(err);
+      return res.render("register");
+    }
+    console.log(user);
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/");
     });
-
-
-router.get("/login",function(req,res){
-    res.render("login");
+  });
 });
 
-router.post("/login",passport.authenticate("local",{
-    successRedirect:"/",
-    failureRedirect:"/register"
-}) ,function(req,res){
-
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
-router.get("/logout",function(req,res){
-    
-    req.logout();
-    res.redirect("/");
-  
+router.post("/login", function (req, res) {
+  const { username, password } = req.body;
+  // console.log(username, password);
+  User.findOne({ username, password }, (err, userauth) => {
+    if (err) return res.redirect("/register");
+    else {
+      if (userauth) {
+        console.log(userauth.username);
+        res.setHeader("set-cookie", [`user=${userauth.username}`]);
+        return res.redirect("/");
+        // return res.render("home", { currentuser: userauth });
+      } else return res.redirect("/register");
+    }
+  });
 });
 
+router.get("/logout", function (req, res) {
+  // req.logout();
+  res.clearCookie("user");
+  res.redirect("/");
+});
 
-
-module.exports=router;
+module.exports = router;
